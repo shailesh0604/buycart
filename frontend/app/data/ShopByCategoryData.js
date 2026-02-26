6d7b89ce2ca1cbf36d8a420ff8ee4d9e730c9e57
Shailesh Bind <Shaileshbind39@gmail.com>
2026-02-27 00:41:34 +0530

    shop by category added in home page

---
+++ frontend/app/data/ShopByCategoryData.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const ShopByCategoryData = [
+    {
+        id: 1,
+        title: "Ultra HD Gaming Monitor",
+        image: "/assets/images/top-deals/monitor.webp",
+        link: "/product/ultra-hd-gaming-monitor",
+    },
+    {
+        id: 2,
+        title: "Lightweight Performance Laptop",
+        image: "/assets/images/top-deals/monitor.webp",
+        link: "/product/performance-laptop",
+    },
+    {
+        id: 3,
+        title: "4K Mirrorless Camera",
+        image: "/assets/images/top-deals/monitor.webp",
+        link: "/product/4k-mirrorless-camera",
+    },
+    {
+        id: 4,
+        title: "Fitness Smartwatch",
+        image: "/assets/images/top-deals/monitor.webp",
+        link: "/product/fitness-smartwatch",
+    },
+    {
+        id: 5,
+        title: "True Wireless Earbuds Pro",
+        image: "/assets/images/top-deals/monitor.webp",
+        link: "/product/tws-earbuds-pro",
+    },
+    {
+        id: 6,
+        title: "Full HD Home Projector",
+        image: "/assets/images/top-deals/monitor.webp",
+        link: "/product/full-hd-projector",
+    },
+    {
+        id: 7,
+        title: "Portable Bluetooth Speaker",
+        image: "/assets/images/top-deals/monitor.webp",
+        link: "/product/portable-bluetooth-speaker",
+    },
+    {
+        id: 8,
+        title: "Wireless All-in-One Printer",
+        image: "/assets/images/top-deals/monitor.webp",
+        link: "/product/wireless-printer",
+    },
+]
+
+export default ShopByCategoryData
+
+
--- frontend/components/Home/ShopByCategory.js
+++ frontend/components/Home/ShopByCategory.js
@@ -1,4 +1,7 @@
+import ShopByCategoryData from '@/app/data/ShopByCategoryData'
+import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
 
 const ShopByCategory = () => {
     return (
@@ -6,10 +9,26 @@ const ShopByCategory = () => {
             <section className='section-shop-by-category'>
                 <div className="custom-container">
                     <div className="section-heading">
-                        <h1 className='section-title'>
+                        <h1 className="section-title">
                             Shop By Category
                         </h1>
                     </div>
+
+                    <div className="shop-by-category-contents">
+                        <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+                            {ShopByCategoryData.map((e) => (
+                                <div key={e.id} className='shop-by-category-content'>
+                                    <Link href={e.link} className='shop-by-category-item flex flex-col gap-4 items-center'>
+                                        <div className="shop-cat-img">
+                                            <Image src={e.image} width={0} height={0} sizes='100%' alt={e.title} />
+                                        </div>
+                                        <h4>{e.title}</h4>
+                                    </Link>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+
                 </div>
             </section>
         </>
